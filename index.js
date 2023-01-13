d89b860c465e1a4883c1eba1cafa175e53337a76
Nicholaschibueze <nicholaschibuezemichael@gmail.com>
2023-01-12 15:36:58 -1100

    OpenAi FrontEnd UI

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+ const path = require ('path');
  const express = require ('express');
  const dotenv = require ('dotenv').config();
  const port = process.env.PORT || 5000;
@@ -8,6 +9,10 @@
 
  app.use(express.json ());
  app.use(express.urlencoded({extended: false})); 
+
+ //  SET STATIC FOLDER 
+
+ app.use(express.static(path.join (__dirname, 'public')));
 
 
  app.use('/openai', require ('./openaiRoutes'));
