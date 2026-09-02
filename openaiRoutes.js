056f21b71bacf89b29284b9f22527f6a32ba3771
Nicholas Chibueze Michael <75815637+Nicholaschibueze@users.noreply.github.com>
2026-09-02 22:34:30 +0100

    Refactor openaiRoutes.js for cleaner code

--- openaiRoutes.js
+++ openaiRoutes.js
@@ -1,12 +1,7 @@
- const express = require ('express');
- const {generateImage} = require ('./controllers/openaiController');
-const req = require('express/lib/request');
-const res = require('express/lib/response');
- const router = express.Router();
+const express = require('express');
+const { generateImage } = require('./controllers/openaiController');
+const router = express.Router();
 
+router.post('/generateimage', generateImage);
 
- router.post('/generateimage', generateImage);
-    
-
-
- module.exports = router;
+module.exports = router;
